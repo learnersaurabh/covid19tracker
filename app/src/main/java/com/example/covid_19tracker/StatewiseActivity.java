ecb936bac1fc91d775c26e6f0425422c04b351cb
ujjwalRajputGit <rajput.nasa@gmail.com>
2020-04-24 22:33:01 +0530

    StatesActivity error resolved

--- app/src/main/java/com/example/covid_19tracker/StatewiseActivity.java
+++ app/src/main/java/com/example/covid_19tracker/StatewiseActivity.java
@@ -12,33 +12,35 @@ public class StatewiseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.states_list);
 
         final ArrayList<Case> cases = new ArrayList<>();
-        cases.add(new Case("Uttar pradesh","2034","564","68"));
-        cases.add(new Case("kashmir","134","54","8"));
-        cases.add(new Case("Delhi","2234","560","60"));
-        cases.add(new Case("Mumbai","4134","1264","98"));
-        cases.add(new Case("Kolkata","1734","864","58"));
-        cases.add(new Case("Telangana","124","64","6"));
-        cases.add(new Case("Rajisthan","5234","1364","268"));
-        cases.add(new Case("Punjab","234","74","18"));
-        cases.add(new Case("Haryana","6234","2004","38"));
-        cases.add(new Case("Uttar pradesh","2034","564","68"));
-        cases.add(new Case("kashmir","134","54","8"));
-        cases.add(new Case("Delhi","2234","560","60"));
-        cases.add(new Case("Mumbai","4134","1264","98"));
-        cases.add(new Case("Kolkata","1734","864","58"));
-        cases.add(new Case("Telangana","124","64","6"));
-        cases.add(new Case("Rajisthan","5234","1364","268"));
-        cases.add(new Case("Punjab","234","74","18"));
-        cases.add(new Case("Haryana","6234","2004","38"));
-        CaseAdapter adapter = new CaseAdapter(this, cases);
+
+        CaseAdapter adapter;
 
         ListView listView = findViewById(R.id.list);
 
-        listView.setAdapter(adapter);
+        cases.add(new Case("Uttar pradesh","2034","564","68"));
+        cases.add(new Case("kashmir","134","54","8"));
+        cases.add(new Case("Delhi","2234","560","60"));
+        cases.add(new Case("Mumbai","4134","1264","98"));
+        cases.add(new Case("Kolkata","1734","864","58"));
+        cases.add(new Case("Telangana","124","64","6"));
+        cases.add(new Case("Rajisthan","5234","1364","268"));
+        cases.add(new Case("Punjab","234","74","18"));
+        cases.add(new Case("Haryana","6234","2004","38"));
+        cases.add(new Case("Uttar pradesh","2034","564","68"));
+        cases.add(new Case("kashmir","134","54","8"));
+        cases.add(new Case("Delhi","2234","560","60"));
+        cases.add(new Case("Mumbai","4134","1264","98"));
+        cases.add(new Case("Kolkata","1734","864","58"));
+        cases.add(new Case("Telangana","124","64","6"));
+        cases.add(new Case("Rajisthan","5234","1364","268"));
+        cases.add(new Case("Punjab","234","74","18"));
+        cases.add(new Case("Haryana","6234","2004","38"));
 
-        setContentView(R.layout.states_list);
+
+        listView.setAdapter(new CaseAdapter(this, cases));
 
     }
 }
